ed43e7c3d37af4900f0774b0d3580c6b14a0e73c
Andrey Kuritsyn <andrew.kuritsyn@gmail.com>
2021-08-14 19:37:36 +0300

    Create user route

--- projects/library/index.js
+++ projects/library/index.js
@@ -2,14 +2,12 @@ const config = require('./config');
 
 const express = require('express');
 const bookRouter = require('./routes/book');
+const userRouter = require('./routes/user');
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/books/', bookRouter);
-
-app.post('/api/user/login', (req, res) => {
-    res.status(201).json({ id: 1, mail: "test@mail.ru" });
-});
+app.use('/api/user/', userRouter);
 
 app.listen(config.port);
