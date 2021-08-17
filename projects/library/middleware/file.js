ef9fc84f7992141c587ef7421a487dc4988d7b5c
Andrey Kuritsyn <andrew.kuritsyn@gmail.com>
2021-08-17 08:52:54 +0300

    Allow only txt or pdf files

--- projects/library/middleware/file.js
+++ projects/library/middleware/file.js
@@ -1,19 +1,15 @@
 const multer = require('multer');
+const config = require('../config');
 
 const storage = multer.diskStorage({
     destination(req, file, cb) {
-        cb(null, 'public/img');
-    },
-    // filename(req, file, cb) {
-    //     cb(null, `${new Date().toISOString().replace(/:/g, '-')}-${file.originalname}`);
-    //     cb(null, `${new Date().toISOString().replace(/:/g, '-')}-${file.originalname}`);
-    // }
+        cb(null, config.uploadDir);
+    }
 });
 
-const allowedTypes = ['text/plain'];
-
-const fileFilter = (req, file, cb) => {
-    if (allowedTypes.includes(file.mimetype)) {
+const allowedTypes = ['text/plain', 'application/pdf'];
+const fileFilter = (_, file, cb) => {
+    if (allowedTypes.includes(file.mimetype) && file.originalname.match(/\.(txt|pdf)$/)) {
         cb(null, true);
     } else {
         cb(null, false);
