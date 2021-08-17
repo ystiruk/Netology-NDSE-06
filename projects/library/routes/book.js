828878746e6c094356fdbdf77e6710ace8437c77
Andrey Kuritsyn <andrew.kuritsyn@gmail.com>
2021-08-17 09:23:48 +0300

    Remove filling fileBook in post, implement upload

--- projects/library/routes/book.js
+++ projects/library/routes/book.js
@@ -1,5 +1,5 @@
-const Book = require('../models/Book');
 const idGenerator = require('node-unique-id-generator');
+const Book = require('../models/Book');
 
 const express = require('express');
 const router = express.Router();
@@ -25,22 +25,40 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    const { title, description, authors, favorite, fileCover, fileName, fileBook } = req.body;
+    const { title, description, authors, favorite, fileCover, fileName } = req.body;
     const id = idGenerator.generateGUID();
-    const newBook = new Book(id, title, description, authors, favorite, fileCover, fileName, fileBook);
+    const newBook = new Book(id, title, description, authors, favorite, fileCover, fileName);
     library.books.push(newBook);
 
     res.status(201).json(newBook);
+});
+
+router.post('/:id/upload', fileMiddleware.single('bookFile'), (req, res) =>
+{
+    const { id } = req.params;
+    const bookIndex = library.books.findIndex(x => x.id === id);
+    if (bookIndex !== -1) {
+        if (!req.file) {
+            //TODO: move error description to middleware
+            return res.status(422).send('Please select a .txt or .pdf to upload');
+        }
+
+        library.books[bookIndex].fileBook = req.file.filename;
+        res.status(201).json();
+    }
+    else {
+        res.status(404).json();
+    }
 });
 
 router.put('/:id', (req, res) => {
     const { id } = req.params;
     const bookIndex = library.books.findIndex(x => x.id === id);
     if (bookIndex !== -1) {
-        const { title, description, authors, favorite, fileCover, fileName, fileBook } = req.body;
+        const { title, description, authors, favorite, fileCover, fileName } = req.body;
         library.books[bookIndex] = {
             ...library.books[bookIndex],
-            title, description, authors, favorite, fileCover, fileName, fileBook
+            title, description, authors, favorite, fileCover, fileName
         };
         res.status(200).json(library.books[bookIndex]);
     } else {
