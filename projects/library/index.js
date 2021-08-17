ed8bdd8fd9889509c61340d80c58ddf84f5d36d9
Andrey Kuritsyn <andrew.kuritsyn@gmail.com>
2021-08-17 18:20:10 +0300

    Create uploads dir if not exist

--- projects/library/index.js
+++ projects/library/index.js
@@ -1,8 +1,10 @@
 const config = require('./config');
-
+const fs = require('fs');
 const express = require('express');
 const bookRouter = require('./routes/book');
 const userRouter = require('./routes/user');
+
+ensureProjectStructureCreated();
 
 const app = express();
 app.use(express.json());
@@ -11,3 +13,11 @@ app.use('/api/books/', bookRouter);
 app.use('/api/user/', userRouter);
 
 app.listen(config.port);
+
+///
+
+function ensureProjectStructureCreated() {
+    if (!fs.existsSync(config.uploadDir)) {
+        fs.mkdirSync(config.uploadDir, { recursive: true });
+    }
+}
