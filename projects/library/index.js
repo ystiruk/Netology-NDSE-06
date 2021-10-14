5d0aace75d2e447b7f90512412d6fb5f45d67dd2
Andrey Kuritsyn <andrew.kuritsyn@gmail.com>
2021-10-14 18:29:00 +0300

    Add mogoose connect to start

--- projects/library/index.js
+++ projects/library/index.js
@@ -34,6 +34,13 @@ function ensureProjectStructureCreated() {
 async function start() {
     try {
         //await mongoose.connect('mongodb://localhost:27017/books_database');
+        await mongoose.connect(config.HOST_DB, {
+            user: config.USER_DB,
+            pass: config.PASSWORD_DB,
+            dbName: config.NAME_DB,
+            userNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         
         app.listen(config.PORT, () => {
             console.log(`=== start server PORT ${config.PORT} ===`);
