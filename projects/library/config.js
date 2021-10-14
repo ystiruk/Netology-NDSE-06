61f396f54c5ae4e365cf4b8e5f59cae7ec43e548
Andrey Kuritsyn <andrew.kuritsyn@gmail.com>
2021-10-14 22:25:21 +0300

    open port 27017 for mongodb

--- projects/library/config.js
+++ projects/library/config.js
@@ -1,7 +1,7 @@
 const PORT = process.env.PORT || 3000;
 const USER_DB = process.env.DB_USERNAME || 'root';
 const PASSWORD_DB = process.env.DB_PASSWORD || 'password';
-const NAME_DB = process.env.DB_NAME || 'books';
+const NAME_DB = process.env.DB_NAME || 'library_db';
 const HOST_DB = process.env.DB_HOST || 'mongodb://localhost:27017/';
 const uploadDir = 'upload/books';
 
